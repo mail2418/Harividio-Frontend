06e9a703c60cb9476542cc2ffb3f845eb590bf1c
Muhammad Ismail <muhammadismail2418@gmail.com>
2022-02-14 19:26:19 +0700

    menambahkan library react dan file components

---
+++ fe_harivideo/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline font-nunito">
+      <p>Harivideo</p>
+    </div>
+  );
+}
+
+export default App;
